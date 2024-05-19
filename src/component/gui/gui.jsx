aa90527493452c0a438ef59e3283cbb7cb0a806c
ishola360 <isholah360.com>
2024-05-19 03:23:46 +0100

    responsiveess

---
+++ src/component/gui/gui.jsx
@@ -0,0 +1,18 @@
+import './gui.css'
+
+function Gui() {
+  return (
+    <div>
+      <div className="theimg">
+        <img src="https://media.istockphoto.com/id/1682172773/photo/a-couple-in-love-having-a-great-time-with-their-cute-little-dachshund-dog.jpg?s=1024x1024&w=is&k=20&c=FxyqTb83Qg5NrPm-UkqNWrBHdCRq24ug5I8gsN3ofhw=" alt="" />
+        <div className="img-cover">
+            <div className="min-title">
+                make the hay
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Gui
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Form from "./component/form/form.jsx";
 import Home from "./component/Home/home.jsx";
 import Generate from "./component/Home/gener.jsx";
+import Gui from "./component/gui/gui.jsx";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
               <Route path="/" element={<Home />} /> 
               <Route path="/register" element={<Form />} />
               <Route path="/qr/:data" element={<Generate />} />
+              <Route path="/gui" element={<Gui/>} />
           </Route>
         </Routes>
     </Router>
